68409ba9ef6237d376b41792c689fcdc0b04392c
wilmerbaby <wilmerbaby@gmail.com>
2022-02-18 21:36:24 -0500

    refactored node js, models, config

--- models/items.js
+++ models/items.js
@@ -1,25 +1,13 @@
 module.exports = function (sequelize, DataTypes) {
-  const Item = sequelize.define("Item", {
-    name: {
-      type: DataTypes.STRING,
-    },
-    count: {
-      type: DataTypes.STRING,
-    },
-  });
-
-  return Item;
-};
-
-// const {Sequelize, DataTypes} = require("sequelize");
-
-// const sequelize = require("../config/connection");
-
-// var Tool = sequelize.define("tool", {
-//     partName: Sequelize.STRING,
-//     department: Sequelize.STRING,
-//     count: Sequelize.INTEGER
-// })
-
-// Tool.sync();
-// module.exports = Tool;
+    const Item = sequelize.define("Item", {
+      name: {
+        type: DataTypes.STRING,
+      },
+      count: {
+        type: DataTypes.INTEGER,
+      },
+    });
+  
+    return Item;
+  };
+  
--- server.js
+++ server.js
@@ -1,30 +1,29 @@
 // Dependencies
 const express = require("express");
+// Create an instance of the express app.
+const app = express();
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+const db = require("./models");
 const routes = require("./controllers/toolController.js");
 //espress handlebars node package. set handlebars
 const exphbs = require("express-handlebars");
 
-// Create an instance of the express app.
-const app = express();
+//middle ware for posting
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
 // handlebars: allowInsecurePrototypeAccess(exphbs)
 
-db = require("./models");
-
 // app.use(express.static("public"));
 
-// process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 8080;
-app.use(routes);
 // var { engine } = require("express-handlebars");
 //Set Handlebars as the default templating engine. parse request body as json
 app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-//middle ware for posting
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(routes);
 
 app.get("/", (req, res) => {
   res.render("frontPage");
@@ -35,37 +34,8 @@ app.get("/api/config", (req, res) => {
   });
 });
 
-
 db.sequelize.sync().then(function () {
   app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
   });
 });
-
-// const res = require("express/lib/response");
-
-// Requiring our models for syncing
-// var db = require("./models/toolTable.js");
-
-// Set the port of our application
-
-
-// app.get("/api/alltools", (req, res)=>{
-//   res.render("allTools")
-// })
-
-// db.sequelize.sync().then(function () {
-
-// });
-
-// Start our server so that it can begin listening to client requests.
-// pp.get("/api/alltools",(req,res)=>{
-
-// });
-
-// router.get("/api/alltools", function (req, res) {
-//   db.findAll({}).then(function (alldbTableTools) {
-//    console.log(alldbTableTools)
-//     res.render("allTools", {alldbTableTools:alldbTableTools});
-
-//   });
